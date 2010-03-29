8d37883e4ceb9e645f8b9c1bbf339c6c9491a1e9
Roland Hieber <rohieb@rohieb.name>
2010-03-29 04:41:09 +0200

    spider is now a class

--- drafts/gc-spider/main.cpp
+++ drafts/gc-spider/main.cpp
@@ -2,26 +2,14 @@
 #include <fstream>
 #include <string>
 #include <cstring>
-#include <cstdlib> 
+#include <cstdlib>
 
 using namespace std;
 
-main(int argc, char * argv[]) {
+int main(int argc, char * argv[]) {
   if (argc < 2) {
     cerr << "not enough parameters!" << endl;
     exit(EXIT_FAILURE);
-  }
-
-  ifstream file(argv[1]);
-  string content, line;
-
-  if(!file) {
-    cerr << "input stream failed!" << endl;
-    exit(EXIT_FAILURE);
-  }
-
-  while(getline(file, line)) {
-    content += line;
   }
 
   // are we logged in?
