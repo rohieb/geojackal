651482c3747787d459d62724608dd67c4de32eb3
Roland Hieber <rohieb@rohieb.name>
2010-04-21 19:53:56 +0200

    Angle: object methods are more fun (and not so much code to write)

--- src/Coordinate.h
+++ src/Coordinate.h
@@ -9,45 +9,69 @@
 namespace geojackal {
 
 /**
- * Helper functions to convert between sexagesimal and decimal values.
+ * Helper class to allow conversion between sexagesimal and decimal values.
  * Example:
  * @code
- * // convert 10°21'15" to decimal: d = 10.35416667
- * double d = Angle::getDecimal(10, 21, 15);
- * double m = Angle::getMinute(d); // m = 21.25
- * double s = Angle::getSecond(d); // s = 15
+ * // create angle with sexagesimal values
+ * Angle a(10, 21, 15);
+ * double d1 = a.deg(); // d = 10.35416667, same as decimal representation
+ * double m = a.min(); // m = 21.25
+ * double s = a.sec(); // s = 15
+ * // or, if you only want the conversion to decimal:
+ * double d2 = Angle(10, 21, 15).deg();
  * @endcode
  */
 struct Angle {
+
+  /** Decimal angle in degree */
+  double degree;
+
   /**
-   * Get the sexagesimal minute of a value
-   * @param val decimal value
-   * @return the sexagesimal minute of the angle
+   * Constructor to set the angle in decimal degree
+   * @param deg decimal angle in degree
    */
-  static inline double getMinute(double val) const {
-    return (int) (val - (int)val) * 60;
+  Angle(double deg = 0) {
+    degree = deg;
   }
 
   /**
-   * Get the sexagesimal seconds of an angle
-   * @param val decimal value
-   * @return the sexagesimal seconds of the angle
-   */
-  static inline double getSecond(double val) const {
-    return (int) (getMinute(val) - (int)getMinute(val)) * 60;
-  }
-
-  /**
-   * Convert sexagesimal value given in degree, minute and second to decimal.
-   * No range checking is done, values for <tt>min</tt> and <tt>sec</tt>
-   * greater than 60 count for the next larger part (i.e. 97' = 1°37').
+   * Constructor to set the angle in sexagesimal degree (consisting of degree,
+   * minute and second). No range checking is done, values for <tt>min</tt>
+   * and <tt>sec</tt> greater than 60 count for the next larger part
+   * (i.e. 97' = 1°37').
    * @param deg sexagesimal degree part
    * @param min sexagesimal minute part
    * @param sec sexagesimal second part
-   * @return decimal value
    */
-  static inline double getDecimal(double deg, double min, double sec = 0) {
-    return deg + (min / 60) + (sec / (60*60));
+  Angle(double deg, double min, double sec = 0) {
+    degree = deg + (min / 60) + (sec / (60*60));
+  }
+
+  /**
+   * Get the decimal/sexagesimal value of the angle in degree. Note that decimal
+   * and sexagesimal values in degree are in fact the same :-)
+   * @return the decimal value of the angle
+   */
+  inline double deg() const {
+    return degree;
+  }
+
+  /**
+   * Get the sexagesimal minute of the angle
+   * @param val decimal value
+   * @return the sexagesimal minute of the angle
+   */
+  inline double min() const {
+    return (int) (deg() - (int)deg()) * 60;
+  }
+
+  /**
+   * Get the sexagesimal seconds of the angle
+   * @param val decimal value
+   * @return the sexagesimal seconds of the angle
+   */
+  inline double sec() const {
+    return (int) (min() - (int)min()) * 60;
   }
 };
 
@@ -95,23 +119,25 @@ struct Coordinate {
     this->lat = lat;
   }
 
-  static QString format(OutputFormat format) const {
+  QString format(OutputFormat format) const {
     QString ns = (lon >= 0) ? "N" : "S";
     QString ew = (lat >= 0) ? "E" : "W";
 
-    double abslon = fabs(lon);
-    double abslat = fabs(lat);
+    Angle abslon(fabs(lon));
+    Angle abslat(fabs(lat));
+
     switch(format) {
       case FORMAT_DEG:
-        return QString("%f %s %f %s").arg(abslon, ns, abslat, ew);
+        return QString("%f %s %f %s").arg(abslon.deg()).arg(ns).
+          arg(abslat.deg()).arg(ew);
       case FORMAT_DEG_MIN:
-        return QString("%d %f %s %d %f %s").arg((int)abslon, Angle::getMinute(
-          abslon), ns, (int)abslat, Angle::getMinute(abslon), ew);
+        return QString("%d %f %s %d %f %s").arg((int)abslon.deg()).
+          arg(abslon.min()).arg(ns).arg((int)abslat.deg()).arg(abslat.min()).
+          arg(ew);
       case FORMAT_DEG_MIN_SEC:
-        return QString("%d %d %f %s %d %d %f %s").arg((int)abslon,
-          (int)Angle::getMinute(abslon), Angle::getSecond(abslon), ns,
-          (int)abslat, (int)Angle::getMinute(abslon), Angle::getSecond(abslon),
-          ew);
+        return QString("%d %d %f %s %d %d %f %s").arg((int)abslon.deg()).
+          arg((int)abslon.min()).arg(abslon.sec()).arg(ns).
+          arg((int)abslat.deg()).arg(abslat.min()).arg(abslat.sec()).arg(ew);
       default:
         return QString("");
     }
