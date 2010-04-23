06786913406801d81e47ffc0879b3228ef1e4225
Roland Hieber <rohieb@rohieb.name>
2010-04-23 20:27:40 +0200

    moved code for Coordinate into single .cpp file (too much code expected)

---
+++ src/Coordinate.cpp
@@ -0,0 +1,49 @@
+/**
+ * @file Coordinate.cpp
+ * @date 23 Apr 2010
+ * @author Roland Hieber <rohieb@rohieb.name>
+ */
+
+#include "Coordinate.h"
+#include <cmath>
+
+using namespace geojackal;
+
+/**
+ * Default constructor. The latitude and longitude parts are set to an
+ * invalid value.
+ */
+Coordinate::Coordinate() :
+  lon(361), lat(361.0) {
+}
+
+/**
+ * Constructor to set latitude and longitude
+ * @param latitude The latitude part of the coordinate, in degree
+ * @param longitude The longitude part of the coordinate, in degree
+ */
+Coordinate::Coordinate(Angle longitude, Angle latitude) :
+  lon(longitude), lat(latitude) {
+}
+
+QString Coordinate::format(OutputFormat format) const {
+  QString ns = (lon >= 0) ? "N" : "S";
+  QString ew = (lat >= 0) ? "E" : "W";
+
+  Angle abslon(fabs(lon));
+  Angle abslat(fabs(lat));
+
+  switch(format) {
+    case FORMAT_DEG:
+      return QString("%f %s %f %s").arg(abslon).arg(ns).arg(abslat).arg(ew);
+    case FORMAT_DEG_MIN:
+      return QString("%d %f %s %d %f %s").arg((int)abslon).arg(abslon.min()).
+        arg(ns).arg((int)abslat).arg(abslat.min()).arg(ew);
+    case FORMAT_DEG_MIN_SEC:
+      return QString("%d %d %f %s %d %d %f %s").arg((int)abslon).arg(
+        (int)abslon.min()).arg(abslon.sec()).arg(ns).arg((int)abslat).arg(
+        abslat.min()).arg(abslat.sec()).arg(ew);
+    default:
+      return QString("");
+  }
+}
--- src/Coordinate.h
+++ src/Coordinate.h
@@ -1,10 +1,7 @@
 #ifndef COORDINATE_H
 #define COORDINATE_H
 
-#include <cmath>
 #include <QString>
-
-/** FIXME Test! */
 
 namespace geojackal {
 
@@ -116,44 +113,9 @@ struct Coordinate {
   /** The latitude (east-west) part of the coordinate, in degree */
   Angle lat;
 
-  /**
-   * Default constructor. The latitude and longitude parts are set to an
-   * invalid value.
-   */
-  Coordinate() :
-    lon(361), lat(361.0) {
-  }
-
-  /**
-   * Constructor to set latitude and longitude
-   * @param latitude The latitude part of the coordinate, in degree
-   * @param longitude The longitude part of the coordinate, in degree
-   */
-  Coordinate(Angle longitude, Angle latitude) :
-    lon(longitude), lat(latitude) {
-  }
-
-  QString format(OutputFormat format) const {
-    QString ns = (lon >= 0) ? "N" : "S";
-    QString ew = (lat >= 0) ? "E" : "W";
-
-    Angle abslon(fabs(lon));
-    Angle abslat(fabs(lat));
-
-    switch(format) {
-      case FORMAT_DEG:
-        return QString("%f %s %f %s").arg(abslon).arg(ns).arg(abslat).arg(ew);
-      case FORMAT_DEG_MIN:
-        return QString("%d %f %s %d %f %s").arg((int)abslon).arg(abslon.min()).
-          arg(ns).arg((int)abslat).arg(abslat.min()).arg(ew);
-      case FORMAT_DEG_MIN_SEC:
-        return QString("%d %d %f %s %d %d %f %s").arg((int)abslon).arg(
-          (int)abslon.min()).arg(abslon.sec()).arg(ns).arg((int)abslat).arg(
-          abslat.min()).arg(abslat.sec()).arg(ew);
-      default:
-        return QString("");
-    }
-  }
+  Coordinate();
+  Coordinate(Angle longitude, Angle latitude);
+  QString format(OutputFormat format) const;
 };
 
 }
