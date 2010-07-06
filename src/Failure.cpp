83f72f943ad9949761fef2640df8ef101a20cfce
Roland Hieber <rohieb@rohieb.name>
2010-07-07 01:41:55 +0200

    Failure class for exceptions with user-defined messages

---
+++ src/Failure.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file Failure.cpp
+ * Generic Failure class with user-defined message
+ * @date Jul 7, 2010
+ * @author Roland Hieber <rohieb@rohieb.name>
+ */
+
+#include "Failure.h"
+
+namespace geojackal {
+
+Failure::Failure(QString& message) :
+  message_(message) {
+}
+
+/** @return Error message given by the thrower */
+const char* Failure::what() const throw() {
+  return message_.toStdString().c_str();
+}
+
+}
---
+++ src/Failure.h
@@ -0,0 +1,31 @@
+/**
+ * @file Failure.h
+ * Generic Failure class with user-defined message
+ * @date Jul 7, 2010
+ * @author Roland Hieber <rohieb@rohieb.name>
+ */
+
+#ifndef FAILURE_H_
+#define FAILURE_H_
+
+#include <exception>
+#include <QString>
+
+namespace geojackal {
+
+/**
+ * Generic Failure class with user-defined message
+ */
+class Failure : public std::exception {
+public:
+  Failure(QString& message);
+  virtual const char* what() const throw();
+
+protected:
+  /** Internal message */
+  QString& message_;
+};
+
+}
+
+#endif /* FAILURE_H_ */
