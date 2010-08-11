70736fb0c1c1ec273112e34faa46314d8ccdd130
Roland Hieber <rohieb@rohieb.name>
2010-08-11 03:07:55 +0200

    LICENSE, README, license headers (GPLv3)

--- src/GCSpiderDialog.cpp
+++ src/GCSpiderDialog.cpp
@@ -2,6 +2,20 @@
  * @file GCSpiderDialog.cpp
  * @date 13 Jul 2010
  * @author Roland Hieber <rohieb@rohieb.name>
+ *
+ * Copyright (C) 2010 Roland Hieber
+ * 
+ * This program is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License, version 3, as published 
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "GCSpiderDialog.h"
